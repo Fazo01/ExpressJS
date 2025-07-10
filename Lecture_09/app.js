5f5f835eb507dd0cae5a2d6c09aa1ed57b173318
Fazo01 <fg7829098@gmail.com>
2025-07-10 06:53:32 +0500

    Lecture_09 Complete

--- Lecture_09/app.js
+++ Lecture_09/app.js
@@ -5,16 +5,21 @@ const express=require('express')
 //local module
 const requestHandler=require('./user')
 const app=express()
-app.use((req,res,next)=>{
-  console.log("Second Middleware",req.url,req.method)
-  res.send('<p>Hello</p>')
+app.use("/",(req,res,next)=>{//Middleware
+  console.log("firsy Middleware",req.url,req.method)
+  // res.send('<p>yalo</p>')
   next()
 })
-app.use((req,res,next)=>{
+app.post("/submit",(req,res,next)=>{
   console.log("Second Middleware",req.url,req.method)
+  res.send('<p>Submit</p>')
+})
+app.use("/",(req,res,next)=>{
+  console.log("third Middleware",req.url,req.method)
+  res.send('<p>Submit 3</p>')
 })
 const server = http.createServer(app)
-const PORT = 3000;
+const PORT = 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
---
+++ Practice Set_09/app.js
@@ -0,0 +1,39 @@
+// const http=require('http')
+const express=require("express")
+const app=express();
+app.use((req,res,next)=>{
+  console.log("First Middleware",req.url,req.method)
+  next()
+})
+app.use((req,res,next)=>{
+  console.log("Second Middleware",req.url,req.method)
+  next()
+})
+// app.get("/contact",(req,res,next)=>{
+//   console.log("Third Middleware",req.url,req.method)
+//   res.send(`<form action="/submit" method="POST">
+//     <input type="text" placeholder="Name" name="name"/>
+//     <input type="text" placeholder="Email" name="Email"
+//     </form>
+//     <button onclick="">Submit</button>`)
+// })
+app.get("/",(req,res,next)=>{
+  console.log("Handling / for GET",req.url,req.method)
+  res.send(`<h1>Welcome</h1>`)
+})
+app.get("/contact-us",(req,res,next)=>{
+  console.log("Handling /contact-us for GET",req.url,req.method)
+  res.send(`<form action="/contact-us" method="POST">
+    <input type="text" placeholder="Name" name="name"/>
+    <input type="text" placeholder="Email" name="Email"
+    </form>
+    <input type="Submit"/>`)
+})
+app.post("/contact-us",(req,res,next)=>{
+  console.log("Handling /contact-us for host GET",req.url,req.method)
+  res.send('<h1>we will contact you shortly</h1>')
+})
+const PORT=3000
+app.listen(PORT,()=>{
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
