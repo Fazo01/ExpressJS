de07c56a9472a9fd6cfc483eddadd7c6f8ce47f6
Fazo01 <fg7829098@gmail.com>
2025-07-12 21:40:15 +0500

    Express JS By angela yu

--- Lecture_09/app.js
+++ Lecture_09/app.js
@@ -5,9 +5,8 @@ const express=require('express')
 //local module
 const requestHandler=require('./user')
 const app=express()
-app.use("/",(req,res,next)=>{//Middleware
+app.use("/",(req,res,next)=>{
   console.log("firsy Middleware",req.url,req.method)
-  // res.send('<p>yalo</p>')
   next()
 })
 app.post("/submit",(req,res,next)=>{
