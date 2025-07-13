cefbdb7e29d3c7c9fef318fb77052f44eee06b29
Fazo01 <fg7829098@gmail.com>
2025-07-13 11:24:04 +0500

    Partially complete

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,3 +1,6 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+const express=require("express")
+const app=express()
+const {} from "path";
