aba1ae83570e585b6676a1973ffc8ba29affecfa
Fazo01 <fg7829098@gmail.com>
2025-07-14 22:18:09 +0500

    Angela express Complete 5

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,6 +1,34 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
-const express=require("express")
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 const app=express()
-const {} from "path";
+const PORT=3000;
+const __dirname=dirname(fileURLToPath(import.meta.url))
+
+app.use(bodyParser.urlencoded({extended:true}))
+function user(req,res,next){
+  const password = req.body["password"];
+  if(password==="ILoveProgramming"){
+    res.locals.userIsAuthorised =true;//new
+  }
+  next()
+}
+app.get("/",(req,res)=>{
+  console.log("Hello",req.url)
+  res.sendFile(__dirname+"/public/index.html")
+})
+
+app.post("/check",user,(req,res)=>{//new
+  if (res.locals.userIsAuthorised){//new
+  res.sendFile(__dirname+"/public/secret.html")
+  }else{
+    res.sendFile(__dirname+"/public/index.html")
+  }
+})
+app.listen(PORT,()=>{
+  console.log(`This is the server link http://localhost:${PORT}`)
+})
