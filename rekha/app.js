6adb1eeda676c6c4f92602e7f2a9d4d11fd8ebde
Fazo01 <fg7829098@gmail.com>
2025-07-17 22:15:09 +0500

    Complete authication process

--- rekha/app.js
+++ rekha/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import bodyParser from "body-parser"
 import {dirname} from "path"
 import {fileURLToPath} from "url"
 const __dirname=dirname(fileURLToPath(import.meta.url))
@@ -10,19 +11,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Authentication
 
 function user(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
+  const {password,email} = req.body
+  // const {email}=req.body["email"];
+  if (password === "admin"&& email==="admin@gmail.com") {
     res.locals.userIsAuthorised = true; //new
   }
   next();
 }
-app.post("/check", user, (req, res) => {
+
+app.post("/dashboard", user, (req, res) => {
   //new
   if (res.locals.userIsAuthorised) {
     //new
-    res.sendFile(__dirname + "/public/secret.html");
+    res.sendFile(__dirname + "/Views/dashboard.html");
   } else {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/Views/login.html");
   }
 });
 
